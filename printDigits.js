216e8a583b9c3b7b5f6c304d20bc5a57a4764be1
Freeman Bird <freemanbird@gmail.com>
2021-11-28 23:21:41 -0700

    challenge complete

--- printDigits.js
+++ printDigits.js
@@ -3,21 +3,27 @@ const printDigits = (num) => {
         console.log(num);
     } else {
         let count = 0;
-        let reduceBy = 1;
-        let moduloBy = 10
+        let boolFlip = false;
         while (num > 0) {
-            if (num % moduloBy === 0) {
+            if (num < 10) {
+                console.log(num);
+                break;
+            }
+            if (num % 10 === 0) {
                 console.log(count);
                 count = 0;
-                reduceBy *= 10;
-                if ((moduloBy * 10) < num) {
-                    moduloBy *= 10;
-                }
+                num /= 10;
+                boolFlip = true;
             }
-            num -= reduceBy;
-            count++;
+            if (!boolFlip) {
+                num--;
+                count++
+            }
+            boolFlip = false;
         }
     }
 }
 
-printDigits(123);
+printDigits(1) // 1
+printDigits(314) // 413
+printDigits(12) // 21
